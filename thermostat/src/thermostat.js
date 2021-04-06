a917cb1f6d16230323f5f9ff37903638c670788b
paulinakoz <paulinakozaczek@gmail.com>
2021-04-06 19:02:43 +0200

    thermostat

--- thermostat/src/thermostat.js
+++ thermostat/src/thermostat.js
@@ -1,14 +1,40 @@
-export class thermostat{
+export class thermostat {
     constructor(){
-        this.temperature = 20
+        this.temperature = 20;
+        this.maxTemp = 25;
     }
+
     decrease(){
-        this.temperature -= 1
+        if(this.temperature >= 10){
+            this.temperature -= 1;
+        } else {
+            return 'Minimum temperature has been reached';
+        }
     }
+
     increase(){
-        this.temperature += 1
+        if(this.temperature <= this.maxTemp){
+            this.temperature += 1;
+        } else {
+            return 'Maximum temperature has been reached';
+        }
     }
+
     get temp(){
-        return this.temperature
+        return this.temperature;
+    }
+
+    powerSavingMode(turnedOn){
+        if(turnedOn === true){
+            this.maxTemp = 25;
+        } else if (turnedOn === false){
+            this.maxTemp = 32;
+        } else {
+            return 'Invalid input';
+        }
+    }
+
+    reset(){
+        this.temperature = 20;
     }
 }
