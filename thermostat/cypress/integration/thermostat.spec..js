fbe2caee6ee9d4e814ca95202a3c43e313710b69
Aydan Awan <aydan.awan@outlook.com>
2021-04-06 17:29:24 +0100

    finished source code

--- thermostat/cypress/integration/thermostat.spec..js
+++ thermostat/cypress/integration/thermostat.spec..js
@@ -4,4 +4,23 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
-describe('')
+import {thermostat} from '../../src/thermostat.js'
+describe('changing the tempeture on the thermostst', function(){
+    it('testing starting temp', function(){
+        let test = new thermostat
+        let output = test.temp
+        expect(output).to.equal(20)
+    })
+    it('testing increase temp', function(){
+        let test = new thermostat
+        test.increase()
+        let output = test.temp
+        expect(output).to.equal(21)
+    })
+    it('testing decrease temp', function(){
+        let test = new thermostat
+        test.decrease()
+        let output = test.temp
+        expect(output).to.equal(19)
+    })
+})
--- thermostat/src/thermostat.js
+++ thermostat/src/thermostat.js
@@ -1,3 +1,14 @@
-class thermostat{
-    
+export class thermostat{
+    constructor(){
+        this.temperature = 20
+    }
+    decrease(){
+        this.temperature -= 1
+    }
+    increase(){
+        this.temperature += 1
+    }
+    get temp(){
+        return this.temperature
+    }
 }
