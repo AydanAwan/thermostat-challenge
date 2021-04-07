ddb65b75c3e1b0f55a2deb1de57858466fd46e7e
paulinakoz <paulinakozaczek@gmail.com>
2021-04-07 15:35:30 +0200

    Update

--- thermostat/cypress/integration/thermostat.spec.js
+++ thermostat/cypress/integration/thermostat.spec.js
@@ -1,9 +1,3 @@
-// thermostat.spec..js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
 import {thermostat} from '../../src/thermostat.js'
 describe('changing the tempeture on the thermostst', function(){
     it('testing starting temp', function(){
