b512f4a4937de863e5de2578106b4bf27ad75bab
Aydan Awan <aydan.awan@outlook.com>
2021-04-08 15:41:40 +0100

    added powersaving not working

--- thermostat/app.js
+++ thermostat/app.js
@@ -1,20 +1,35 @@
-import {thermostat} from './src/thermostat.js'
+import {Thermostat} from './src/thermostat.js'
+let savingbutonstate = true
+window.addEventListener('load', function() {
+    const thermostat = new Thermostat()
+
+    const tempElement = document.getElementById("temperature")
+    const upButton = document.getElementById("up-button")
+    const downButton = document.getElementById("down-button")
+    const resetButton = document.getElementById("reset-button")
+    const powersavingbutton = document.getElementById("powersaving-button");
 
 
-const Thermostat = new thermostat()
-const tempElement = document.getElementById("temperature")
-const upButton = document.getElementById("up-button")
-const downButton = document.getElementById("down-button")
-const resetButton = document.getElementById("reset-button")
-window.addEventListener('load',function(){
-upButton.addEventListener("click",function(){
-    tempElement.innerHTML = Thermostat.increase()
+    upButton.addEventListener("click",function(){
+        tempElement.innerHTML = thermostat.increase()
+    })
+    downButton.addEventListener("click",function(){
+        tempElement.innerHTML = thermostat.decrease()
+    })
+    resetButton.addEventListener("click",function(){
+        tempElement.innerHTML = thermostat.reset()
+    })
+    powersavingbutton.addEventListener("click",function(){
+        tempElement.innerHTML = thermostat.powerSavingMode()
+        // if(savingbutonstate === true){
+        //     property.style.backgroundColor = "#FFFFFF"
+        //     savingbutonstate = false;
+        // } else if (savingbutonstate === false){
+        //     property.style.backgroundColor = "#7FFF00"
+        //     savingbutonstate = true;
+        // }
+    })
+
+    tempElement.innerHTML = thermostat.temperature;
+
 })
-downButton.addEventListener("click",function(){
-    tempElement.innerHTML = Thermostat.decrease()
-})
-resetButton.addEventListener("click",function(){
-    tempElement.innerHTML = Thermostat.reset()
-})
-})
-tempElement.innerHtML = Thermostat.temperature
--- thermostat/cypress/integration/thermostat.spec.js
+++ thermostat/cypress/integration/thermostat.spec.js
@@ -1,24 +1,24 @@
-import {thermostat} from '../../src/thermostat.js'
+import {Thermostat} from '../../src/thermostat.js'
 describe('changing the tempeture on the thermostst', function(){
     it('testing starting temp', function(){
-        let test = new thermostat
+        let test = new Thermostat
         let output = test.temp
         expect(output).to.equal(20)
     })
     it('testing increase temp', function(){
-        let test = new thermostat
+        let test = new Thermostat
         test.increase()
         let output = test.temp
         expect(output).to.equal(21)
     })
     it('testing decrease temp', function(){
-        let test = new thermostat
+        let test = new Thermostat
         test.decrease()
         let output = test.temp
         expect(output).to.equal(19)
     })
     it('testing decrease temp past min', function(){
-        let test = new thermostat
+        let test = new Thermostat
         for(let i =0; i <10; i++){
             test.decrease()
         }
@@ -26,7 +26,7 @@ describe('changing the tempeture on the thermostst', function(){
         expect(output).to.equal('Minimum temperature has been reached')
     })
     it('testing increase temp past power saver temp when on', function(){
-        let test = new thermostat
+        let test = new Thermostat
         for(let i =0; i <5; i++){
             test.increase()
         }
@@ -34,7 +34,7 @@ describe('changing the tempeture on the thermostst', function(){
         expect(output).to.equal('Maximum temperature has been reached')
     })
     it('testing increase temp past power saver temp when off', function(){
-        let test = new thermostat
+        let test = new Thermostat
         test.powerSavingMode(false)
         for(let i =0; i <6; i++){
             test.increase()
@@ -43,26 +43,26 @@ describe('changing the tempeture on the thermostst', function(){
         expect(output).to.equal(26)
     })
     it('testing reseting temp', function(){
-        let test = new thermostat
+        let test = new Thermostat
         test.decrease()
         test.reset()
         let output = test.temp
         expect(output).to.equal(20)
     })
     it('testing low usage temp', function(){
-        let test = new thermostat
+        let test = new Thermostat
         test.temperature = 16
         let output = test.colour()
         expect(output).to.equal('low usage')
     })
     it('testing medium usage temp', function(){
-        let test = new thermostat
+        let test = new Thermostat
         test.temperature = 21
         let output = test.colour()
         expect(output).to.equal('medium usage')
     })
     it('testing high usage temp', function(){
-        let test = new thermostat
+        let test = new Thermostat
         test.temperature = 300
         let output = test.colour()
         expect(output).to.equal('high usage')
